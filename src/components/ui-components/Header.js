034cc1b384ab4db970f664bd15d50ea147201636
jdfcitecky <jdfcitecky@gmail.com>
2022-06-08 12:51:51 +0800

    add search bar

--- src/components/ui-components/Header.js
+++ src/components/ui-components/Header.js
@@ -12,6 +12,10 @@ export default class Header extends Component {
                             <a className="blog-header-logo text-dark" href="#">Hsin's Portfilio</a>
                         </div>
                         <div className="col-4 d-flex justify-content-end align-items-center">
+                            <div class="input-group input-group-sm">
+
+                                <input type="text" class="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
+                            </div>
                             <a className="text-muted" href="#">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
                             </a>
