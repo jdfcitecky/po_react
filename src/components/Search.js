f4689fe65e36e0d0a596a0f6e29b52b68545bd61
jdfcitecky <jdfcitecky@gmail.com>
2022-06-09 15:47:52 +0800

    add cardwork component

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import Jumbotron from './ui-components/Jumbotron';
-import Card from './ui-components/Card';
+import CardWork from './ui-components/CardWork';
 import Timeline from './ui-components/Timeline';
 export default class Home extends Component {
     constructor(props) {
         super(props)
         this.state = {
             searchValue: this.props.match.params.value,
+            works: [{ color: "primary", category: "Backend", title: "Default", date: "2022-06-09", text: "AAAA", id: "1" }, { color: "primary", category: "Backend", title: "Default", date: "2022-06-09", text: "AAAA", id: "2" }],
         }
         // this.handleChange = this.handleChange.bind(this)
         // this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     render() {
+        var works = this.state.works
         return (
             <div>
                 <div className='container'>
@@ -22,6 +24,9 @@ export default class Home extends Component {
                         </div>
                     </div>
                 </div>
+                {works.map((w) => (
+                    <CardWork color={w.color} category={w.category} title={w.title} date={w.date} text={w.text} id={w.id} />
+                ))}
             </div>
         );
     }
---
+++ src/components/ui-components/CardWork.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+export default class CardWork extends Component {
+
+    constructor(props) {
+        super(props)
+
+    }
+    render() {
+        return (
+            <div className="col-md-6">
+                <div className="card flex-md-row mb-4 box-shadow h-md-250">
+                    <div className="card-body d-flex flex-column align-items-start">
+                        <strong className={`d-inline-block mb-2 text-${this.props.color}`}>{this.props.category}</strong>
+                        <h3 className="mb-0">
+                            <a className="text-dark" href="#">{this.props.title}</a>
+                        </h3>
+                        <div className="mb-1 text-muted">{this.props.date}</div>
+                        <p className="card-text mb-auto">{this.props.text}</p>
+                        <a href={`/work/${this.props.id}`}>Read more...</a>
+                    </div>
+                    <img className="card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap" />
+                </div>
+            </div>
+        );
+    }
+}
