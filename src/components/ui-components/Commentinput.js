753b7d29700383782b9044712c82a7a50b9f2b46
jdfcitecky <jdfcitecky@gmail.com>
2022-06-09 15:22:51 +0800

    work page finish without api

--- src/components/ui-components/Commentinput.js
+++ src/components/ui-components/Commentinput.js
@@ -4,7 +4,9 @@ export default class Commentinput extends Component {
 
     constructor(props) {
         super(props)
-
+        this.state = {
+            textValue: "",
+        }
     }
     render() {
         return (
@@ -12,7 +14,7 @@ export default class Commentinput extends Component {
                 <div className="col-md-12">
                     <div class="coment-bottom bg-white p-2 px-4">
                         <div class="d-flex flex-row add-comment-section mt-4 mb-4">
-                            <input type="text" class="form-control mr-3" placeholder="Add comment" />
+                            <input value={this.state.textValue} onChange={(event) => this.setState({ textValue: event.target.value })} type="text" class="form-control mr-3" placeholder="Add comment" />
                             <button class="btn btn-primary my-2" type="button">Comment</button>
                         </div>
                     </div>
