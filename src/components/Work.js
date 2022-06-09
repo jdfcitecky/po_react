c4f3da1126de4247bab7a5c01e13c693ddd87c79
jdfcitecky <jdfcitecky@gmail.com>
2022-06-09 13:23:03 +0800

    add introduce in work

--- src/components/Work.js
+++ src/components/Work.js
@@ -7,6 +7,18 @@ import Jumbotron from './ui-components/Jumbotron';
 import Card from './ui-components/Card';
 import Timeline from './ui-components/Timeline';
 export default class Work extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            id: this.props.match.params.id,
+            title: "Title",
+            text: "default text default text default text default text default text default text default text default text",
+            tools: "python, mysql, golang, javascript",
+            downloadlink: "#",
+        }
+        // this.handleChange = this.handleChange.bind(this)
+        // this.handleSubmit = this.handleSubmit.bind(this)
+    }
     render() {
         var settings = {
             dots: true,
@@ -30,7 +42,6 @@ export default class Work extends Component {
                     href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
                 />
                 <div className='container'>
-                    {/* <h1>work</h1> */}
                     <div className='row'>
                         <div className='col-12'>
                             <Slider {...settings}>
@@ -47,6 +58,22 @@ export default class Work extends Component {
                                     <img src="http://placekitten.com/g/1000/400" />
                                 </div>
                             </Slider>
+                        </div>
+                    </div>
+                    <div className='row mt-5'>
+                        <div className='col-6 text-left'>
+                            <h1 className="display-5">{this.state.title}</h1>
+                            <p>{this.state.text}</p>
+                            <p><a class="btn btn-secondary" href={this.state.downloadlink} role="button">Download &raquo;</a></p>
+                        </div>
+                        <div className='col-6'>
+                            <div className="bg-light mr-md-1 pt-1 px-1 pt-md-1 px-md-1 text-left overflow-hidden">
+                                <div className="my-3 p-3">
+                                    <h5 >Tools</h5>
+                                    <p className="lead">{this.state.tools}</p>
+                                </div>
+                                {/* <div className="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px; border-radius: 21px 21px 0 0;"></div> */}
+                            </div>
                         </div>
                     </div>
 
