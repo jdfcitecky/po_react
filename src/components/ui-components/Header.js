d0b67e79e0ab602c143cfb98e6dc044afd41003b
jdfcitecky <jdfcitecky@gmail.com>
2022-06-10 13:17:48 +0800

    update some links at navbar

--- src/components/ui-components/Header.js
+++ src/components/ui-components/Header.js
@@ -26,7 +26,7 @@ export default class Header extends Component {
                 <header className="blog-header py-3">
                     <div className="row flex-nowrap justify-content-between align-items-center">
                         <div className="col-4 pt-1">
-                            <a className="text-muted" href="#">Subscribe</a>
+                            <a className="text-muted" href="/manage">Manage</a>
                         </div>
                         <div className="col-4 text-center">
                             <a className="blog-header-logo text-dark" href="/">Hsin's Portfilio</a>
--- src/components/ui-components/Navbar.js
+++ src/components/ui-components/Navbar.js
@@ -5,12 +5,12 @@ export default class Navbar extends Component {
         return (
             <div className="nav-scroller py-1 mb-2">
                 <nav className="nav d-flex justify-content-between">
-                    <a className="p-2 text-muted" href="#">Home</a>
-                    <a className="p-2 text-muted" href="#">Forntend</a>
-                    <a className="p-2 text-muted" href="#">Backend</a>
-                    <a className="p-2 text-muted" href="#">M.S.Project</a>
-                    <a className="p-2 text-muted" href="#">Design</a>
-                    <a className="p-2 text-muted" href="#">Contact</a>
+                    <a className="p-2 text-muted" href="/">Home</a>
+                    <a className="p-2 text-muted" href="/search/frontend">Forntend</a>
+                    <a className="p-2 text-muted" href="/search/backend">Backend</a>
+                    <a className="p-2 text-muted" href="/search/msproject">M.S.Project</a>
+                    <a className="p-2 text-muted" href="/search/design">Design</a>
+                    <a className="p-2 text-muted" href="/">Contact</a>
                 </nav>
             </div>
         );
