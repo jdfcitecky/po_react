4f0520fccb4ac1613540b1fdad2b4d692452ec43
jdfcitecky <jdfcitecky@gmail.com>
2022-06-11 13:14:05 +0800

    fix link at home cards

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ export default class Home extends Component {
                 <div className='container'>
                     <Jumbotron />
                     <div className='row mb-2'>
-                        <Card color='primary' category='Frontend' text='aaaaaa' link='#' linkwords='See more' />
-                        <Card color='success' category='Backend' text='aaaaaa' link='#' linkwords='See more' />
+                        <Card color='primary' category='Frontend' text='aaaaaa' link='/search/frontend' linkwords='See more' />
+                        <Card color='success' category='Backend' text='aaaaaa' link='/search/backend' linkwords='See more' />
                     </div>
                     <div className='row mb-2'>
-                        <Card color='info' category='M.S Project' text='aaaaaa' link='#' linkwords='See more' />
-                        <Card color='warning' category='Design' text='aaaaaa' link='#' linkwords='See more' />
+                        <Card color='info' category='M.S Project' text='aaaaaa' link='/search/msproject' linkwords='See more' />
+                        <Card color='warning' category='Design' text='aaaaaa' link='/search/design' linkwords='See more' />
                     </div>
 
                 </div>
