b0a26c533c73b6fcc18aeb2fa09b3b50e5f1e97c
jdfcitecky <jdfcitecky@gmail.com>
2022-06-12 23:13:39 +0800

    fix all warning at date 0612

--- src/components/ui-components/Header.js
+++ src/components/ui-components/Header.js
@@ -61,7 +61,7 @@ export default class Header extends Component {
                                 <input id="searchBar" value={this.state.searchValue} onChange={(event) => this.setState({ searchValue: event.target.value })} type="text" className="form-control" aria-label="Small" aria-describedby="inputGroup-sizing-sm" />
                             </div>
                             <Link className="text-muted" to={`/search/${this.state.searchValue}`}>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" className="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mx-3"><circle cx="10.5" cy="10.5" r="7.5"></circle><line x1="21" y1="21" x2="15.8" y2="15.8"></line></svg>
                             </Link>
 
                             {loginLink}
