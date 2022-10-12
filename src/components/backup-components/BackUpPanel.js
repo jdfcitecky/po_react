9d3a6d2d43e1d65d588f1a8275820ef85328fbe6
jdfcitecky <jdfcitecky@gmail.com>
2022-10-12 12:59:39 +0800

    ready to write articles

--- src/components/backup-components/BackUpPanel.js
+++ src/components/backup-components/BackUpPanel.js
@@ -21,9 +21,23 @@ export default class BackUpPanel extends Component {
                 picturefour: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
                 picturefive: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
 
+            }, {
+                id: -1,
+                title: "Title",
+                category: "Backend",
+                text: "Set the direction of flex items in a flex container with direction utilities. In most cases you can omit the horizontal class here as the browser default is row. However, you may encounter situations where you needed to explicitly set this value (like responsive layouts).",
+                tools: "python mysql golang javascript",
+                year: "2020",
+                downloadlink: "#",
+                pictureone: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
+                picturetwo: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
+                picturethree: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
+                picturefour: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
+                picturefive: `http://${process.env.REACT_APP_API_ADDRESS}/static/backup/a.jpg`,
+
             },],
             worksShow: [],
-            isLoaded: false,
+            isLoaded: true,
             error: null,
             errors: [],
             isManager: this.props.isManager,
@@ -117,6 +131,17 @@ export default class BackUpPanel extends Component {
         return (
 
             <div>
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    charSet="UTF-8"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+                />
+                <link
+                    rel="stylesheet"
+                    type="text/css"
+                    href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+                />
                 <link href="/docs/4.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
                 <div className='container'>
                     <div className='row'>
@@ -124,29 +149,45 @@ export default class BackUpPanel extends Component {
                         <main role="main" className="col-md-9 ml-sm-auto col-lg-10 px-4">
                             <div className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
                                 <h1 className="h2">Backup</h1>
+                                <div className='btn btn-primary'>Initail</div>
                             </div>
 
                             {worksShow.map((w) => (
-                                <div className="col-md-12 fadeIn">
-                                    <div className="card flex-md-row mb-4 box-shadow h-md-250">
-                                        <div className="card-body d-flex flex-column align-items-start">
-                                            <div class="d-flex flex-row">
-                                                <strong className={`d-inline-block mb-2 text-${w.color}`}>{w.category}</strong>
-                                                <span className="dot mx-2 my-2"></span><span className=" mr-2">{w.year}</span>
+                                <div className="my-3 card col-md-12 fadeIn">
+                                    <div className='row'>
+                                        <div className='col-8'>
+                                            <div className="card-body d-flex flex-column align-items-start">
+                                                <div class="d-flex flex-row">
+                                                    <strong className={`d-inline-block mb-2 text-${w.color}`}>{w.category}</strong>
+                                                    <span className="dot mx-2 my-2"></span><span className=" mr-2">{w.year}</span>
+                                                </div>
+                                                <h3 className="mb-0">
+                                                    <p className="text-dark">{w.title}</p>
+                                                </h3>
+                                                <p className="backup-card-text">{w.text}</p>
+
                                             </div>
-                                            <h3 className="mb-0">
-                                                <p className="text-dark">{w.title}</p>
-                                            </h3>
-                                            <p className="backup-card-text">{w.text}</p>
-                                            <div class="d-flex flex-row">
-                                                {w.toolTags.map((t) => (
-                                                    <span class="badge badge-info mx-1">{t}</span>
-                                                ))}
+                                        </div>
+                                        <div className='col-4 backup-card-tools'>
+                                            <div className='d-flex flex-column'>
+                                                <div class="d-flex flex-row">
+                                                    {w.toolTags.map((t) => (
+                                                        <span class="badge badge-info mx-1">{t}</span>
+                                                    ))}
+                                                </div>
                                             </div>
 
                                         </div>
+                                    </div>
+                                    <div className="card p-2 mb-4 box-shadow h-md-250 d-flex justify-content-between">
+                                        <div class="pt-4 d-flex justify-content-between">
+                                            <div className='backup-previewZone'><img src={w.pictureone} /></div>
+                                            <div className='backup-previewZone'><img src={w.picturetwo} /></div>
+                                            <div className='backup-previewZone'><img src={w.picturethree} /></div>
+                                            <div className='backup-previewZone'><img src={w.picturefour} /></div>
+                                            <div className='backup-previewZone'><img src={w.picturefive} /></div>
+                                        </div>
 
-                                        <img className="backup-card-img card-img-right flex-auto d-none d-md-block" data-src="holder.js/200x250?theme=thumb" alt="Card image cap" />
                                     </div>
                                 </div>
                             ))}
