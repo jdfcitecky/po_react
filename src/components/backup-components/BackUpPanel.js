f17150fb734215ce043c1bcbea1ae7f41dae3d86
jdfcitecky <jdfcitecky@gmail.com>
2022-10-21 11:10:29 +0800

    modify a work 1110 1021

--- src/components/backup-components/BackUpPanel.js
+++ src/components/backup-components/BackUpPanel.js
@@ -102,7 +102,7 @@ export default class BackUpPanel extends Component {
                 //-------
                 {
                     id: -1,
-                    title: "TODO",
+                    title: "TODODota week rest",
                     category: "Frontend",
                     text: "This project is a very early work when I was learning to make Web Apps. Compared with the Flask, I chose to learn the more complicated Django first. In this project, I used a fairly simple application to demonstrate my understanding of MVC. Architecture. In this project, in order to display news headlines on the homepage, first create the corresponding article model, then define the routing and control function, and use this function to call the data to display in the view template through ORM library. However, due to the lack of knowledge of html and css at that time, the results were extremely simple, which also prompted me to further study the front-end and back-end related knowledge.",
                     tools: "Python xlwings beautifulsoup Excel LineBOTAPI",
@@ -116,7 +116,7 @@ export default class BackUpPanel extends Component {
 
                 }, {
                     id: -1,
-                    title: "TODO",
+                    title: "TODODota week rest",
                     category: "Frontend",
                     text: "本專案是我在學習製作Web App時相當初期的作品，相較於較為輕量的Flask，我選擇先學習較為複雜的Django，而在這個專案中即用了相當簡單的應用展示了我對於MVC架構的理解。在這個專案中，為了在首頁展示新聞標題，先建立對應的文章模型(Model)，接著定義路由與控制(Control)函數，並藉由這個函數透過ORM的方式調用資料顯示於視圖(View)模板中。但由於當時缺乏html與css的知識，因此在成果上極為簡陋，也促使我想更進一步學習前後端相關知識。",
                     tools: "Python Django SQLite",
