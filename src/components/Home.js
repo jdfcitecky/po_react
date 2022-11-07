82467d6f92908db77ee19fc7d0ab9b66565f50f9
jdfcitecky <jdfcitecky@gmail.com>
2022-11-07 13:19:48 +0800

    modify the card text at home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,12 +9,12 @@ export default class Home extends Component {
                 <div style={{ height: document.documentElement.clientHeight - 200 }} className='container fadeIn'>
                     <Jumbotron />
                     <div className='row mb-2'>
-                        <Card color='primary' category='Backend' text='aaaaaa' link='/search/backend' linkwords='See more' picture='' />
-                        <Card color='success' category='Frontend' text='aaaaaa' link='/search/frontend' linkwords='See more' picture='' />
+                        <Card color='primary' category='Backend' text='Using Go, Python, NodeJS and the corresponding framework to implement various API micro-services including instant messaging, membership system, mall system, database operation, file server, crawler, chat robot, etc., and deploy on AWS with Docker.' link='/search/backend' linkwords='See more' picture='' />
+                        <Card color='success' category='Frontend' text='Using React to create a variety of interactive web pages that are easy to maintain and make good use of CSS to efficiently meet various design needs. Create web games and image processing apps using Javascript Canvas.' link='/search/frontend' linkwords='See more' picture='' />
                     </div>
                     <div className='row mb-2'>
-                        <Card color='info' category='M.S Project' text='aaaaaa' link='/search/m.s.%20project' linkwords='See more' picture='' />
-                        <Card color='warning' category='Design' text='aaaaaa' link='/search/design' linkwords='See more' picture='' />
+                        <Card color='info' category='M.S Project' text='Projects completed during the CS Master class at UC Davis, including blockchain-based asset trading platform, RISCV-based game console operating system, Ethernet protocol simulation, scientific computing experimental simulation, linear algebra numerical method research, etc.' link='/search/m.s.%20project' linkwords='See more' picture='' />
+                        <Card color='warning' category='Design' text='Through architectural design and product design projects, demonstrate the capabilities in project design and management, including research on user and environmental analysis, formulation of product functions and work plans, optimization and product cycle management.' link='/search/design' linkwords='See more' picture='' />
                     </div>
 
                 </div>
--- src/components/ui-components/Card.js
+++ src/components/ui-components/Card.js
@@ -13,7 +13,7 @@ export default class Card extends Component {
             <div className="col-md-6 fadeIn">
                 <div className="card flex-md-row mb-4 box-shadow h-md-250">
                     <div className="home-card-textZone card-body d-flex flex-column align-items-start">
-                        <strong className={`d-inline-block mb-2 text-${this.props.color}`}>{this.props.category}</strong>
+                        <strong className={`d-inline-block mb-0 text-${this.props.color}`}>{this.props.category}</strong>
                         <h3 className="mb-0">
                             <p className="text-dark  home-card-title" href="#">{this.props.title}</p>
                         </h3>
