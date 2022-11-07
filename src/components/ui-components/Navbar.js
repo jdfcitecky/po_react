b11582be0f6b6928c496d4de4151235e577f8931
jdfcitecky <jdfcitecky@gmail.com>
2022-11-07 13:56:00 +0800

    fix the bug that navbar will not reset the page of search

--- src/components/ui-components/Navbar.js
+++ src/components/ui-components/Navbar.js
@@ -6,12 +6,12 @@ export default class Navbar extends Component {
         return (
             <div className="nav-scroller py-1 mb-2">
                 <nav className="nav d-flex justify-content-between">
-                    <Link className="p-2 text-muted" to="/">Home</Link>
-                    <Link className="p-2 text-muted" to="/timeline">Time line</Link>
-                    <Link className="p-2 text-muted" to="/search/frontend">Forntend</Link>
-                    <Link className="p-2 text-muted" to="/search/backend">Backend</Link>
-                    <Link className="p-2 text-muted" to="/search/m.s.%20project">M.S.Project</Link>
-                    <Link className="p-2 text-muted" to="/search/design">Design</Link>
+                    <Link className="p-2 text-muted" to="/" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>Home</Link>
+                    <Link className="p-2 text-muted" to="/timeline" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>Time line</Link>
+                    <Link className="p-2 text-muted" to="/search/frontend" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>Forntend</Link>
+                    <Link className="p-2 text-muted" to="/search/backend" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>Backend</Link>
+                    <Link className="p-2 text-muted" to="/search/m.s.%20project" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>M.S.Project</Link>
+                    <Link className="p-2 text-muted" to="/search/design" onClick={() => { window.localStorage.setItem("searchPage", String(1)) }}>Design</Link>
 
                 </nav>
             </div>
