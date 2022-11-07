150a54dd7f860829019979c95ba978cf84a8713c
jdfcitecky <jdfcitecky@gmail.com>
2022-11-07 13:44:02 +0800

    modify footer

--- src/components/ui-components/Footer.js
+++ src/components/ui-components/Footer.js
@@ -28,7 +28,7 @@ export default class Footer extends Component {
                 <div style={{ "height": "150px" }}></div>
                 <footer className="navbar-fixed-bottom blog-footer fixed-bottom">
 
-                    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
+                    <p>Hsin's protfolio by <a href="mailto:jdfcitecky@gmail.com">jdfcitecky@gmail.com</a>.</p>
                     <p>
                         <a href="" className='text-primary' onClick={() => { document.documentElement.scrollTop = 0 }}>Back to top</a>
                     </p>
