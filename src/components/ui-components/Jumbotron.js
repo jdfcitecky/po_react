4cb77f966369b3d04564af0ad4ce635c7f0b75ad
jdfcitecky <jdfcitecky@gmail.com>
2022-11-08 11:34:13 +0800

    TODO add to jubotron

--- src/components/ui-components/Jumbotron.js
+++ src/components/ui-components/Jumbotron.js
@@ -5,7 +5,7 @@ export default class Jumbotron extends Component {
             <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
                 <div className="col-md-6 px-0">
                     <h1 className="display-4 font-italic">Hi I'am Chuan Hsin Cho</h1>
-                    <p className="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what's most interesting in this post's contents.</p>
+                    <p className="lead my-3">TODO</p>
                     <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
                 </div>
             </div>
