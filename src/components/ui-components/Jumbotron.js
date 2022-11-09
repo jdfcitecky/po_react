ec2e28aa2186c6a498a3e560f2201c9ecee140de
jdfcitecky <jdfcitecky@gmail.com>
2022-11-09 11:20:44 +0800

    add prop text to jubotron

--- src/components/ui-components/Jumbotron.js
+++ src/components/ui-components/Jumbotron.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 export default class Jumbotron extends Component {
+    constructor(props) {
+        super(props)
+
+    }
     render() {
         return (
             <div className="jumbotron p-3 p-md-5 text-white rounded bg-dark">
                 <div className="col-md-6 px-0">
-                    <h1 className="display-4 font-italic">Hi I'am Chuan Hsin Cho</h1>
-                    <p className="lead my-3">TODO</p>
+                    <h1 className="display-4 font-italic">Hi I'am ChuanHsin Cho</h1>
+                    <p className="lead my-3">{this.props.text}</p>
                     <p className="lead mb-0"><a href="#" className="text-white font-weight-bold">Continue reading...</a></p>
                 </div>
             </div>
